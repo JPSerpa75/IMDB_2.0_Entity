5d254e77d1d78db52291a29fd81bf4392aca3918
Wiris Rafael <wiris.wernek@itixti.com.br>
2023-12-04 02:55:51 -0300

    feat: add fluxo de atuação

---
+++ IMDB 2.0 - Entity/Controllers/AtuacoesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IMDB_2._0___Entity.Context;
+using IMDB_2._0___Entity.Models;
+
+namespace IMDB_2._0___Entity.Controllers
+{
+    public class AtuacoesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Atuacoes
+        public ActionResult Index()
+        {
+            var atuacoes = context.atuacoes.Include(a => a.ator).Include(f => f.filme).OrderBy(n => n.atuacaoId);
+            return View(atuacoes);
+        }
+
+        // GET: Atuacoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.AtorId = new SelectList(context.atores.OrderBy(a => a.nome), "AtorId", "Nome");
+            ViewBag.FilmeId = new SelectList(context.Filmes.OrderBy(f => f.titulo), "FilmeId", "Titulo");
+            return View();
+        }
+
+        // POST: Atuacoes/Create
+        [HttpPost]
+        public ActionResult Create(Atuacao atuacao)
+        {
+            context.atuacoes.Add(atuacao);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        // GET: Atuacoes/Delete
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.
+                BadRequest);
+            }
+            Atuacao atuacao = context.atuacoes.Where(a => a.atuacaoId ==
+            id).Include(at => at.ator).Include(f => f.filme).
+            First();
+            if (atuacao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atuacao);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(long id)
+        {
+            Atuacao atuacao = context.atuacoes.Find(id);
+            context.atuacoes.Remove(atuacao);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- IMDB 2.0 - Entity/Models/Ator.cs
+++ IMDB 2.0 - Entity/Models/Ator.cs
@@ -7,8 +7,9 @@ namespace IMDB_2._0___Entity.Models
 {
     public class Ator
     {
-        public int atorId { get; set; }
+        public long atorId { get; set; }
         public String nome { get; set; }
         public String sobrenome { get; set; }
+        public virtual ICollection<Atuacao> atuacoes { get; set; }
     }
 }
